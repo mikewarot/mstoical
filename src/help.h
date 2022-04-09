d65298c6114f99c55bd83e97b07178b67eac0195
Mike Warot <chezmike@gmail.com>
2022-04-09 15:55:23 -0500

    Fix multiline string in src/help.h

--- src/help.h
+++ src/help.h
@@ -1,18 +1,18 @@
-const char help[] = "S t o i c a l  %s 
-Copyright (c) 2002 Jonathan Moore Liles.
-Released under the GNU General Public License (see COPYING)
-
-usage:
-stoical [ option ] ... [ file ] [ arg ] ... 
-
-The available options include:
-
--h			- Print this help and exit.
--v			- Print version information and exit.
--e code			- Exectue the instructions 'code'.
--l path			- Change STOICAL root library path.
--f file			- Run 'file' (useful when file begins with a '-').
-
+const char help[] = "S t o i c a l  %s \n\
+Copyright (c) 2002 Jonathan Moore Liles.\n\
+Released under the GNU General Public License (see COPYING)\n\
+\n\
+usage:\n\
+stoical [ option ] ... [ file ] [ arg ] ... \n\
+\n\
+The available options include:\n\
+\n\
+-h			- Print this help and exit.\n\
+-v			- Print version information and exit.\n\
+-e code			- Exectue the instructions 'code'.\n\
+-l path			- Change STOICAL root library path.\n\
+-f file			- Run 'file' (useful when file begins with a '-').\n\
+\n\
 ";
 
 
