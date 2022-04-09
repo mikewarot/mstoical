7b1e540e0948b509394d5a1b5c9ba3cf0c837290
nomadbyte <nomadbyte@users.noreply.github.com>
2022-04-09 00:19:10 -0500

    Fix help.h

--- src/help.h
+++ src/help.h
@@ -1,18 +1,18 @@
-const char help[] = "S t o i c a l  %s 
-Copyright (c) 2002 Jonathan Moore Liles.
-Released under the GNU General Public License (see COPYING)
-
-usage:
-stoical [ option ] ... [ file ] [ arg ] ... 
-
-The available options include:
-
--h			- Print this help and exit.
--v			- Print version information and exit.
--e code			- Exectue the instructions 'code'.
--l path			- Change STOICAL root library path.
--f file			- Run 'file' (useful when file begins with a '-').
-
-";
+const char help[] = "S t o i c a l  %s "
+"\nCopyright (c) 2002 Jonathan Moore Liles.\n"
+"\nReleased under the GNU General Public License (see COPYING)"
+"\n"
+"\nusage:"
+"\nstoical [ option ] ... [ file ] [ arg ] ... "
+"\n"
+"\nThe available options include:"
+"\n"
+"\n-h			- Print this help and exit."
+"\n-v			- Print version information and exit."
+"\n-e code			- Exectue the instructions 'code'."
+"\n-l path			- Change STOICAL root library path."
+"\n-f file			- Run 'file' (useful when file begins with a '-')."
+"\n"
+;
 
 
